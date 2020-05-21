7c00a20ff87f3a2dea0c498f19ee35f9a6f7f5fc
MISIU <michalj0721@gmail.com>
2020-05-21 20:52:10 +0200

    master

---
+++ my-app/src/components/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './Form';
+import Result from './Result';
+
+//Key for app
+const APIKey = `6aaee9475bfa8671b59d63ce163d4cdd`
+
+class App extends Component {
+  
+  state = {
+    value: "",
+    date: "",
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: false,
+  }
+  
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleCitySubmit = e => {
+    e.preventDefault()
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}`
+    fetch(API)
+    .then(response => {
+      if(response.ok) {
+        return response
+      }
+      throw Error('Nie udało się')
+    })
+    .then(response => response.json() )
+    .then(data => {
+      const time = new Date().toLocaleTimeString()
+      this.setState( prevState => ({
+        err: false,
+        date: time,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        temp: data.main.temp,
+        pressure: data.main.pressure,
+        wind: data.wind.speed,
+        city: prevState.value,
+      }))
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState( prevState => ({
+        err: true,
+        city: prevState.value,
+      }))
+    })
+
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <Form 
+            value={this.state.value} 
+            change = {this.handleInputChange} 
+            submit = {this.handleCitySubmit}
+          />
+          <Result weather={this.state} />
+        </div>
+      );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/Result.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Result = props => {
+
+    const { err, city, sunrise, sunset, temp, pressure, wind, date } = props.weather
+    
+    let content = null;
+    
+    if(!err && city) {
+
+        const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
+        const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
+        content = (
+            <div>
+                <h2>Wyszukiwanie dla miasta: <em>{city}</em></h2>
+                <h3>Dane dla dnia i godziny: {date}</h3>  
+                <h3>Aktualna temperatura: {temp}</h3> 
+                <h3>Wschód słońca dzisiaj o: {sunriseTime}</h3> 
+                <h3>Zachód słońca dzisiaj o: {sunsetTime}</h3>
+                <h3>Aktualna siła wiatru: {wind} m/s</h3>
+                <h3>Aktualne ciśnienie: {pressure} hPa</h3>
+            </div>
+        )
+    }
+
+    return( 
+        <div className="result">
+            {err ? `Nie mamy w bazie: ${city}` : content}
+        </div>
+     );
+}
+
+export default Result;
